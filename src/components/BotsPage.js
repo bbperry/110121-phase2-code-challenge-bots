e0294366aec8934e4af8531ffc596c39c6158bda
Ben <benbperry1@gmail.com>
2021-12-03 09:06:40 -0800

    firstcommit

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
+const API = 'http://localhost:8002/bots'
+
 function BotsPage() {
-  //start here with your code for step one
+
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => setBots(data));
+  }, []);
 
   return (
     <div>
